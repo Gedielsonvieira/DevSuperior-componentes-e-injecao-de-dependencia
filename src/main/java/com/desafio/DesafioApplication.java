701bf73f8725eca99578754c2923a5a1c43fc463
Gedielson Vieira <gedielsonvieira@outlook.com>
2023-06-01 22:15:11 -0300

    Criado os pedidos, ajuste na saída e na lógica do método shipment

--- src/main/java/com/desafio/DesafioApplication.java
+++ src/main/java/com/desafio/DesafioApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class DesafioApplication implements CommandLineRunner {
 
@@ -21,9 +23,20 @@ public class DesafioApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Order order = new Order(1034, 150.00, 20.0);
-        System.out.println("Pedido código: " + order.getCode());
-        System.out.println("Valor total: " + orderService.total(order));
+        Order order1 = new Order(1034, 150.00, 20.0);
+        Order order2 = new Order(2282, 800.00, 10.0);
+        Order order3 = new Order(1309, 95.90, 0.0);
+
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Pedido código " + order1.getCode());
+        System.out.printf("Valor total: R$ %.2f\n\n", orderService.total(order1));
+
+        System.out.println("Pedido código " + order2.getCode());
+        System.out.printf("Valor total: R$ %.2f\n\n", orderService.total(order2));
+
+        System.out.println("Pedido código " + order3.getCode());
+        System.out.printf("Valor total: R$ %.2f", orderService.total(order3));
     }
 
 }
--- src/main/java/com/desafio/services/ShippingService.java
+++ src/main/java/com/desafio/services/ShippingService.java
@@ -8,7 +8,7 @@ public class ShippingService {
     public double shipment(Order order) {
         if (order.getBasicValue() < 100) {
             return 20.00;
-        } else if (order.getBasicValue() >= 100 | order.getBasicValue() <= 200) {
+        } else if (order.getBasicValue() >= 100 & order.getBasicValue() <= 200) {
             return 12.00;
         } else {
             return 0.0;
